a2b9588cc823abf3b70e5f9689d4562ff76a7cfc
eosfel97 <91592659+eosfel97@users.noreply.github.com>
2022-02-15 22:17:45 +0100

    registration

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,11 +4,11 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -17,32 +17,82 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
+
+
+            ->add('username', TextType::class, [
+                'required' => true,
+                'attr' => [
+                    'placeholder' => "Username",
+                ],'constraints' => [
+                    new NotBlank([
+                        'message' => "Vous devez indiquer votre unsername.",
                     ]),
                 ],
             ])
+            ->add('email', EmailType::class, [
+                'required' => true,
+                'attr' => [
+                    'placeholder' => "Email",
+                ],'constraints' => [
+                    new NotBlank([
+                        'message' => "L'email {{ valeur }} n'est pas un email valide.",
+                    ]),
+                ],
+            ])
+            ->add('lastname', TextType::class, [
+                'required' => true,
+                'attr' => [
+                    'placeholder' => "Nom de famille",
+                ],'constraints' => [
+                    new NotBlank([
+                        'message' => "Vous devez indiquer votre nom.",
+                    ]),
+                ],
+            ])
+            ->add('firstname', TextType::class, [
+                'required' => true,
+                'attr' => [
+                    'placeholder' => "Prénom",
+                ],new NotBlank([
+                        'message' => "Vous devez indiquer votre prénom.",
+                    ]),
+            ])
+            
             ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => [
+                    'placeholder' => "Mot de passe",
+                    'autocomplete' => 'new-password',
+                ],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => "Vous devez indiquer un mot de passe.",
                     ]),
                     new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
+                        'min' => 8,
+                        'minMessage' => "Votre mot de passe doit comporter au moins {{ limite }} caractères.",
                     ]),
                 ],
             ])
+            ->add('phone', TextType::class, [
+                'required' => true,
+                'attr' => [
+                    'placeholder' => "Numéro de téléphone",
+                ],new NotBlank([
+                        'message' => "Vous devez indiquer votre numéro de téléphone.",
+                    ]),
+            ])
+            ->add('address', TextType::class, [
+                'required' => true,
+                'attr' => [
+                    'placeholder' => "Adresse postale",
+                ],new NotBlank([
+                        'message' => "Vous devez indiquer votre adresse postale.",
+                    ]),
+            ])
+            
+
+
         ;
     }
 
